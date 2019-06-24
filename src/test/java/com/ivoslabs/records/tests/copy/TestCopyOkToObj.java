6790061d73c0fff9ab1bd88533fc593b396a5a5c
Iván Pérez Cóyotl <iperez.c@outlook.com>
2019-06-23 20:50:57 -0500

    Update TestCopyOkToObj.java

--- src/test/java/com/ivoslabs/records/tests/copy/TestCopyOkToObj.java
+++ src/test/java/com/ivoslabs/records/tests/copy/TestCopyOkToObj.java
@@ -23,7 +23,7 @@ public class TestCopyOkToObj {
     public void testToObject1() {
 	CopyParser cex = new CopyParser();
 
-	CopyDataDTO cdto = cex.toObject("b2301.220190306-    ", CopyDataDTO.class);
+	CopyDataDTO cdto = cex.toObject("b2301.220190306    ", CopyDataDTO.class);
 
 	String expected = "CopyDataDTO [field1=b, field2=2, field3=3, field4=false, field5=1.2, field6=Wed Mar 06 00:00:00 CST 2019, field7=null]";
 	String actual = cdto.toString();
@@ -53,21 +53,6 @@ public class TestCopyOkToObj {
     public void testToObject3() {
 	CopyParser cex = new CopyParser();
 
-	CopyDataDTO cdto = cex.toObject("b-301.220190306aa,bb", CopyDataDTO.class);
-
-	String expected = "CopyDataDTO [field1=b, field2=null, field3=3, field4=false, field5=1.2, field6=Wed Mar 06 00:00:00 CST 2019, field7=SubField [a=aa, b=bb]]";
-	String actual = cdto.toString();
-
-	System.out.println(expected);
-	System.out.println(actual);
-
-	assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testToObject4() {
-	CopyParser cex = new CopyParser();
-
 	CopyDataDTO cdto = cex.toObject("b 301.220190306aa,bb", CopyDataDTO.class);
 
 	String expected = "CopyDataDTO [field1=b, field2=null, field3=3, field4=false, field5=1.2, field6=Wed Mar 06 00:00:00 CST 2019, field7=SubField [a=aa, b=bb]]";
@@ -78,14 +63,15 @@ public class TestCopyOkToObj {
 
 	assertEquals(expected, actual);
     }
+ 
 
     @Test
     public void testToObjects() {
 	CopyParser cex = new CopyParser();
 
 	List<String> crows = new ArrayList<String>();
-	crows.add("a2304.520190306-    ");
-	crows.add("b-301.220190306aa,bb");
+	crows.add("a2304.520190306     ");
+	crows.add("b 301.220190306aa,bb");
 
 	List<CopyDataDTO> cdtos = cex.toObjects(crows, CopyDataDTO.class);
 
