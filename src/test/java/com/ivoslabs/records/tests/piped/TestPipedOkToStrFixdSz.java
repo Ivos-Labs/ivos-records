9272de9d6dcade19c6e9aaba53910544b0db2c22
Iván Pérez Cóyotl <iperez.c@outlook.com>
2020-02-18 16:57:31 -0600

    test cases

--- src/test/java/com/ivoslabs/records/tests/piped/TestPipedOkToStrFixdSz.java
+++ src/test/java/com/ivoslabs/records/tests/piped/TestPipedOkToStrFixdSz.java
@@ -22,63 +22,99 @@ import com.ivoslabs.records.tests.piped.dtos.PipedDataFSsDTO;
  *
  */
 public class TestPipedOkToStrFixdSz {
+
     @Test
     public void testToString() {
-	PipedParser ex = new PipedParser();
+        
+        PipedParser ex = new PipedParser();
 
-	PipedDataFSsDTO dto = new PipedDataFSsDTO();
-	dto.setField1(null);
-	dto.setField2(2);
-	dto.setField3(1);
-	dto.setField4(false);
-	dto.setField5(2.2);
+        PipedDataFSsDTO dto = new PipedDataFSsDTO();
+        dto.setField5("");
+        dto.setField6("");
+        dto.setField7("");
+        dto.setField8("");
+        //
+        dto.setField9(123456);
+        dto.setField10(123456);
+        dto.setField11(123);
+        dto.setField12(456);
+        //
 
-	try {
-	    dto.setField6(new DateLatinConverver().toObject("20190306"));
-	} catch (ParseException e) {
-	    e.printStackTrace();
-	}
-	dto.setField7(new SubField("aa", "bb"));
+        dto.setField16(2);
+        //
+        dto.setField17(1);
+        dto.setField18(false);
+        dto.setField19(2.2);
 
-	String expected = "|2|1|false       |2.2            |            20190306|aa,bb";
-	String actual = ex.toString(dto);
+        try {
+            dto.setField20(new DateLatinConverver().toObject("20190306"));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        
+        dto.setField21(new SubField("aa", "bb"));
 
-	assertEquals(expected, actual);
+        String expected = "||   |   |||   |   |123456|123|123  |  456|||   |  2|1|false       |2.2            |            20190306|aa,bb";
+        String actual = ex.toString(dto);
+
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testToStrings() {
-	PipedParser ex = new PipedParser();
+        PipedParser ex = new PipedParser();
 
-	PipedDataFSsDTO dto1 = new PipedDataFSsDTO();
-	dto1.setField1("1");
-	dto1.setField2(2);
-	dto1.setField3(1);
-	dto1.setField4(true);
-	dto1.setField5(2.2);
+        PipedDataFSsDTO dto1 = new PipedDataFSsDTO();
+        dto1.setField5("");
+        dto1.setField6("");
+        dto1.setField7("");
+        dto1.setField8("");
+        //
+        dto1.setField9(123456);
+        dto1.setField10(123456);
+        dto1.setField11(123);
+        dto1.setField12(456);
+        //
 
-	PipedDataFSsDTO dto = new PipedDataFSsDTO();
-	dto.setField1("b");
-	dto.setField2(2);
-	dto.setField3(1);
-	dto.setField4(false);
-	dto.setField5(2.2);
+        dto1.setField16(2);
+        //
+        dto1.setField17(1);
+        dto1.setField18(false);
+        dto1.setField19(2.2);
 
-	try {
-	    dto.setField6(new DateLatinConverver().toObject("20190306"));
-	} catch (ParseException e) {
-	    e.printStackTrace();
-	}
+        PipedDataFSsDTO dto = new PipedDataFSsDTO();
+        dto.setField5("");
+        dto.setField6("");
+        dto.setField7("");
+        dto.setField8("");
+        //
+        dto.setField9(123456);
+        dto.setField10(123456);
+        dto.setField11(123);
+        dto.setField12(456);
+        //
 
-	List<String> dtos = ex.toStrings(Arrays.asList(dto1, dto));
+        dto.setField16(2);
+        //
+        dto.setField17(1);
+        dto.setField18(false);
+        dto.setField19(2.2);
 
-	String expected = "b|2|1|false       |2.2            |            20190306|";
+        try {
+            dto.setField20(new DateLatinConverver().toObject("20190306"));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
-	String actual = null;
-	for (String string : dtos) {
-	    actual = string;
-	}
+        List<String> dtos = ex.toStrings(Arrays.asList(dto1, dto));
 
-	assertEquals(expected, actual);
+        String expected = "||   |   |||   |   |123456|123|123  |  456|||   |  2|1|false       |2.2            |            20190306|";
+
+        String actual = null;
+        for (String string : dtos) {
+            actual = string;
+        }
+
+        assertEquals(expected, actual);
     }
 }
