27d5c6b52f3bdef1d6a5e041b8d55c29d9e3defe
Iván Pérez Cóyotl <iperez.c@outlook.com>
2021-01-20 19:16:28 -0600

    Update RowConsumer.java

--- src/main/java/com/ivoslabs/records/core/func/RowConsumer.java
+++ src/main/java/com/ivoslabs/records/core/func/RowConsumer.java
@@ -1,12 +1,14 @@
 /**
- * 
+ *
  */
 package com.ivoslabs.records.core.func;
+
+import org.apache.commons.lang3.mutable.MutableBoolean;
 
 /**
  * Interface to be implemented to process a file row<br>
  * Evaluate if the line is a header, data or tail<br>
- * 
+ *
  * @author www.ivoslabs.com
  *
  */
@@ -14,11 +16,12 @@ public interface RowConsumer {
 
     /**
      * Evaluate if the line is a header, data or tail en execute the respetive Parser
-     * 
+     *
      * @param row       line content
      * @param rowNumber row number
+     * @param keep      flag to stop
      * @throws Exception
      */
-    void process(String row, int rowNumber) throws Exception;
+    void process(String row, int rowNumber, MutableBoolean keep) throws Exception;
 
 }
