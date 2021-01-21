97bd7f6221d77d68b84c8b9aa349a75eab540caa
Iván Pérez Cóyotl <iperez.c@outlook.com>
2021-01-20 19:16:31 -0600

    Update CopyParser.java

--- src/main/java/com/ivoslabs/records/parsers/CopyParser.java
+++ src/main/java/com/ivoslabs/records/parsers/CopyParser.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.ivoslabs.records.parsers;
 
@@ -12,7 +12,7 @@ import com.ivoslabs.records.function.Consumer;
 
 /**
  * Utility to parse COPY data to POJOs (using {@code @Pic} annotation) and viceversa
- * 
+ *
  * @author www.ivoslabs.com
  * @see com.ivoslabs.records.annontation.Pic &#64;Pic
  * @see com.ivoslabs.records.annontation.Pic#beginIndex() &#64;Pic#beginIndex
@@ -35,34 +35,34 @@ public class CopyParser {
 
     /**
      * Creates a new instance of T using the received data
-     * 
-     * @param      <T> Required type
+     *
+     * @param <T>  Required type
      * @param data values separated by pipes
      * @param type required type
      * @return a T instance
      */
     public <T> T toObject(String data, Class<T> type) {
-	return Extractor.convertStringToObject(data, type, Pic.class);
+        return Extractor.convertStringToObject(data, type, Pic.class);
     }
 
     /**
      * Creates a new instance of T using each item in the received data list
-     * 
-     * @param      <T> Required type
+     *
+     * @param <T>  Required type
      * @param data list of values separated by pipes
      * @param type required type
      * @return a List of T instance
      */
     public <T> List<T> toObjects(List<String> data, Class<T> type) {
-	return Extractor.convertStringsToObjects(data, type, Pic.class);
+        return Extractor.convertStringsToObjects(data, type, Pic.class);
     }
 
     /**
      * Creates an instance of H, D, or T for each row in a file and execute the respective received ObjectConsumer
-     * 
-     * @param                <H> Header type
-     * @param                <D> Data type
-     * @param                <T> Tail type
+     *
+     * @param <H>            Header type
+     * @param <D>            Data type
+     * @param <T>            Tail type
      * @param file           file path
      * @param headerType     header type
      * @param headerSize     number of first rows to be processed with headerType and headerConsumer
@@ -74,26 +74,26 @@ public class CopyParser {
      * @param tailConsumer   action to do for each tail instance
      */
     public <H, D, T> void processFile(String file,
-	    // header
-	    Class<H> headerType,
-	    Integer headerSize,
-	    Consumer<H> headerConsumer,
-	    // data
-	    Class<D> dataType,
-	    Consumer<D> dataConsumer,
-	    // tail
-	    Class<T> tailType,
-	    Integer tailSize,
-	    Consumer<T> tailConsumer) {
+            // header
+            Class<H> headerType,
+            Integer headerSize,
+            Consumer<H> headerConsumer,
+            // data
+            Class<D> dataType,
+            Consumer<D> dataConsumer,
+            // tail
+            Class<T> tailType,
+            Integer tailSize,
+            Consumer<T> tailConsumer) {
 
-	Extractor.processFile(file, headerType, headerSize, headerConsumer, dataType, dataConsumer, tailType, tailSize, tailConsumer, Pic.class);
+        Extractor.processFile(file, headerType, headerSize, headerConsumer, dataType, dataConsumer, null, tailType, tailSize, tailConsumer, Pic.class);
     }
 
     /**
      * Creates an instance of H or T for each row in a file and execute the respective received ObjectConsumer
-     * 
-     * @param                <H> Header type
-     * @param                <D> Data type
+     *
+     * @param <H>            Header type
+     * @param <D>            Data type
      * @param file           file path
      * @param headerType     header type
      * @param headerSize     number of first rows to be processed with headerType and headerConsumer
@@ -102,22 +102,22 @@ public class CopyParser {
      * @param dataConsumer   action to do for each data instance
      */
     public <H, D> void processFile(String file,
-	    // header
-	    Class<H> headerType,
-	    Integer headerSize,
-	    Consumer<H> headerConsumer,
-	    // data
-	    Class<D> dataType,
-	    Consumer<D> dataConsumer) {
+            // header
+            Class<H> headerType,
+            Integer headerSize,
+            Consumer<H> headerConsumer,
+            // data
+            Class<D> dataType,
+            Consumer<D> dataConsumer) {
 
-	this.processFile(file, headerType, headerSize, headerConsumer, dataType, dataConsumer, null, null, null);
+        this.processFile(file, headerType, headerSize, headerConsumer, dataType, dataConsumer, null, null, null);
     }
 
     /**
      * Creates an instance of D or T for each row in a file and execute the respective received ObjectConsumer
-     * 
-     * @param              <D> Data type
-     * @param              <T> Tail type
+     *
+     * @param <D>          Data type
+     * @param <T>          Tail type
      * @param file         file path
      * @param dataType     data type
      * @param dataConsumer action to do for each data instance
@@ -126,27 +126,27 @@ public class CopyParser {
      * @param tailConsumer action to do for each tail instance
      */
     public <D, T> void processFile(String file,
-	    // Data
-	    Class<D> dataType,
-	    Consumer<D> dataConsumer,
-	    // Tail
-	    Class<T> tailType,
-	    Integer tailSize,
-	    Consumer<T> tailConsumer) {
+            // Data
+            Class<D> dataType,
+            Consumer<D> dataConsumer,
+            // Tail
+            Class<T> tailType,
+            Integer tailSize,
+            Consumer<T> tailConsumer) {
 
-	this.processFile(file, null, null, null, dataType, dataConsumer, tailType, tailSize, tailConsumer);
+        this.processFile(file, null, null, null, dataType, dataConsumer, tailType, tailSize, tailConsumer);
     }
 
     /**
      * Creates an instance of D for each row in a file and execute the respective received ObjectConsumer
-     * 
-     * @param              <D> Data type
+     *
+     * @param <D>          Data type
      * @param file         file path
      * @param dataType     fata type
      * @param dataConsumer action to do for each data instance
      */
     public <D> void processFile(String file, Class<D> dataType, Consumer<D> dataConsumer) {
-	this.processFile(file, null, null, null, dataType, dataConsumer, null, null, null);
+        this.processFile(file, null, null, null, dataType, dataConsumer, null, null, null);
     }
 
     /********************************
@@ -167,22 +167,22 @@ public class CopyParser {
 
     /**
      * Creates a {@code String} object representing the received object
-     * 
+     *
      * @param data the {@code Object} to be converted to a {@code Strings}
      * @return a string representation of the value of the received object
      */
     public String toString(Object data) {
-	return Extractor.convertObjectToString(data, Pic.class);
+        return Extractor.convertObjectToString(data, Pic.class);
     }
 
     /**
      * Creates a {@code String} object representing each item in the received object list
-     * 
+     *
      * @param data the {@code Object} list to be converted to a {@code Strings}
      * @return a list of string representation of the value of each item in the received object list
      */
     public List<String> toStrings(List<?> data) {
-	return Extractor.convertObjectsToStrings(data, Pic.class);
+        return Extractor.convertObjectsToStrings(data, Pic.class);
     }
 
     /********************************
@@ -203,53 +203,53 @@ public class CopyParser {
 
     /**
      * Creates a {@code String} object representing each item in the received object lists and append it into a file
-     * 
-     * @param        <H> Header type
-     * @param        <D> Data type
-     * @param        <T> Tail type
+     *
+     * @param <H>    Header type
+     * @param <D>    Data type
+     * @param <T>    Tail type
      * @param file   path file to save string representations of the value of each item in the received object lists
      * @param header objects to be appended into the fist rows of the file
      * @param data   objects to be appended into after header rows
      * @param tails  objects to be appended into the last rows of the file
      */
     public <H, D, T> void objectsToFile(String file, Stack<H> header, Stack<D> data, Stack<T> tails) {
-	Extractor.convertObjectsToFile(file, header, data, tails, Pic.class);
+        Extractor.convertObjectsToFile(file, header, data, tails, Pic.class);
     }
 
     /**
      * Creates a {@code String} object representing each item in the received object lists and append it into a file
-     * 
-     * @param        <H> Header type
-     * @param        <D> Data type
+     *
+     * @param <H>    Header type
+     * @param <D>    Data type
      * @param file   path file to save string representations of the value of each item in the received object lists
      * @param header objects to be appended into the fist rows of the file
      * @param data   objects to be appended into after header rows
      */
     public <H, D> void objectsToFileHD(String file, Stack<H> header, Stack<D> data) {
-	this.objectsToFile(file, header, data, null);
+        this.objectsToFile(file, header, data, null);
     }
 
     /**
      * Creates a {@code String} object representing each item in the received object lists and append it into a file
-     * 
-     * @param       <D> Data type
-     * @param       <T> Tail type
+     *
+     * @param <D>   Data type
+     * @param <T>   Tail type
      * @param file  path file to save string representations of the value of each item in the received object lists
      * @param data  objects to be appended into after header rows
      * @param tails objects to be appended into the last rows of the file
      */
     public <D, T> void objectsToFileDT(String file, Stack<D> data, Stack<T> tails) {
-	this.objectsToFile(file, null, data, tails);
+        this.objectsToFile(file, null, data, tails);
     }
 
     /**
      * Creates a {@code String} object representing each item in the received object list and append it into a file
-     * 
-     * @param      <D> Data type
+     *
+     * @param <D>  Data type
      * @param file path file to save string representations of the value of each item in the received object list
      * @param data objects to be appended into the file
      */
     public <D> void objectsToFile(String file, Stack<D> data) {
-	this.objectsToFile(file, null, data, null);
+        this.objectsToFile(file, null, data, null);
     }
 }
